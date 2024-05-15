a0195151ccc4b020817bbb7185ec348748afd019
CLARANGO <167893189+CLARANGO@users.noreply.github.com>
2024-05-15 17:13:07 +0800

    Update project 2.sql

--- project 2.sql
+++ project 2.sql
@@ -6,11 +6,18 @@ select * from bigquery-public-data.thelook_ecommerce.order_items
 select
 FORMAT_DATE('%Y-%m',delivered_at) as month_year,
 count(order_id) as total_user,
-count(status) as total_order
+sum (case when status = 'Complete' then 1 else 0 end) as total_order,
 from bigquery-public-data.thelook_ecommerce.order_items
-where extract(YEAR from delivered_at) || '-' || extract(MONTH from delivered_at) between'2019-1' and '2020-4'
-and status = 'Complete'
+where FORMAT_DATE('%Y-%m',delivered_at) between'2019-01' and '2020-04'
 group by month_year
 order by month_year
+
+--There was a consistent growth of more than 200 in the number of buyers from Jan 2019 to April 2020. In addition, the total number of completed orders between Jan 2019 and April 2020 was stable at above 50% of the total number of orders.
+
+
+
+
+
+
 
 
